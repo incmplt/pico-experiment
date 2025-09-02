a4df6dc6e4c21635131e43c41727f0523d83451e
Yasushi ABE <yasushi.abe@infocircus.jp>
2025-09-02 17:36:17 +0900

    103JT-025 Thermistor(サーミスター)を使用した温度測定コード。若干誤差があるかも…

---
+++ micropython/thermistor/README.md
@@ -0,0 +1,22 @@
+# Raspberry Pi Pico : 103JT-025/050 Thermistor
+
+![RaspberryPi Pico Thermistor](thermistor-unit-001.png)
+
+## Requirement
+
+* RaspberryPi Pico
+* 超薄型サーミスター 103JT-025 : [AE-AQM0802+PCA9515](https://akizukidenshi.com/catalog/g/g110160/)
+* カーボン抵抗(炭素皮膜抵抗) 1/4W10kΩ : [カーボン抵抗(炭素皮膜抵抗) 1/4W10kΩ](https://akizukidenshi.com/catalog/g/g125103/)
+
+## Reference
+
+[RaspberryPi Pico Setup:incmplt](https://www.incmplt.net/2022/09/10/raspberrypi-pico-setup/)
+
+## Licence
+
+[MIT](https://github.com/tcnksm/tool/blob/master/LICENCE)
+
+## Author
+
+* [incmplt](https://www.incmplt.net/)
+* [Info Circus,Inc.](https://www.infocircus.jp/)
---
+++ micropython/thermistor/main.py
@@ -0,0 +1,42 @@
+from picozero import pico_led
+import machine
+import time
+import math
+
+sensor=machine.ADC(0)
+sensor_temp = machine.ADC(4)
+conversion_factor = 3.3 / (65535)
+
+B = 3950.0
+R0 = 10000.0
+T0 = 25.0
+
+def readThermistorTemp( adc_val ):
+    # ADC値から電圧を計算
+    volt = adc_val * conversion_factor   
+    # 電圧からサーミスタ抵抗を計算(r)
+    r = (( 3.3 / volt ) - 1.0 ) * R0
+    # 抵抗値から係数を計算    
+    B0 = 3452.9 * math.pow( r, -0.012329 )
+    # サーミスタ抵抗から温度を計算
+    T_bar = (1.0 / B) * math.log( r / R0 ) + (1 / ( T0 + 273.15 ))    
+    # 計算結果はケルビン温度の逆数になっているので摂氏に戻す
+    T = (1.0 / T_bar) - 273.15
+    return T
+
+while True :
+    pico_led.on()
+    time.sleep(1)
+    reading = sensor_temp.read_u16() * conversion_factor
+    # The temperature sensor measures the Vbe voltage of a biased bipolar diode, connected to the fifth ADC channel
+    # Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree.
+    temperature = 27 - (reading - 0.706)/0.001721
+
+    sens = sensor.read_u16()
+    templ = readThermistorTemp( sens )
+
+    print( "----+----+----+" )
+    print( temperature )
+    print( templ )
+    pico_led.off()
+    time.sleep(1)
